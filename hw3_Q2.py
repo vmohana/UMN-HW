d2d4280aa71e5d384c6a0a1973790b31d9b2fe52
vmohana <36269935+vmohana@users.noreply.github.com>
2019-10-30 23:43:23 -0500

    Update hw3_Q2.py

--- hw3_Q2.py
+++ hw3_Q2.py
@@ -1,12 +1,25 @@
 '''
 NAME: MOHANA KRISHNA VUTUKURU
-HOMEWORK 3
-Driver script for homework 3
+DRIVER SCRIPT - HOMEWORK 3
 '''
 
+import numpy as np
+from sklearn.cluster import KMeans
 from skimage import io
+import matplotlib.pyplot as plt
 
+# Read the image
 img = io.imread('stadium.bmp')
-img = img/255
+img = img[:,:,:3]/255
 
-print(type(img))
+# Reshape image
+original_shape = img.shape
+pixels = np.reshape(img, (img.shape[0]*img.shape[1], 3))
+
+# Fit the KMeans
+colors = KMeans(n_clusters=7)
+colors = colors.fit(pixels)
+compressed_image = colors.cluster_centers_[colors.labels_]
+compressed_image = np.reshape(compressed_image, (original_shape))
+plt.imshow(compressed_image)
+plt.show()

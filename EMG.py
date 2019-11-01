7178ebeb8853eac75c4c1f95003d285196198d59
vmohana <36269935+vmohana@users.noreply.github.com>
2019-11-01 12:02:00 -0500

    Update EMG.py

--- EMG.py
+++ EMG.py
@@ -1,30 +1,36 @@
-'''
-INTRODUCTION TO MACHINE LEARNING
-NAME: MOHANA KRISHNA VUTUKURU
-EXPECTATION MAXIMIZATION
-'''
-# 1. Run KMeans
-# 2. 
 from skimage import io
-from skimage.viewer import ImageViewer
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
+from scipy.stats import multivariate_normal
 
 def EMG(image, k, flag):
 
     # Turn the image into pixels
-    image = image[:,:,:3]/255
-    pixels = np.reshape(image[:,:,:3], (image.shape[0]*image.shape[1], 3))    
-    colors = KMeans(n_clusters=k).fit(pixels)
-    clusters = np.hstack((pixels, np.reshape(colors.labels_, (13400,1))))
-    
-    data_dict, means, variances = {}, {}, {}
-    for i in range(k):
-        data_dict[i] = clusters[clusters[:,-1] == i][:,:-1]
-        means[i] = np.mean(data_dict[i], axis = 0)
-        variances[i] = np.cov(data_dict[i], rowvar=False)
-    return variances
+    if flag == 0:
+	    image = image[:,:,:3]/255
+	    pixels = np.reshape(image[:,:,:3], (image.shape[0]*image.shape[1], 3))    
+	    colors = KMeans(n_clusters=k).fit(pixels)
+	    clusters = np.hstack((pixels, np.reshape(colors.labels_, (13400,1))))
+	    initial_means = colors.cluster_centers_
 
+	    # Store the initial variances and pi values indexed by gaussian.
+	    initial_variances, initial_pi = [], []
+	    for i in range(k):
+	        initial_variances.append(np.cov(clusters[clusters[:,-1]==i][:,:-1], rowvar = False))
+	        initial_pi.append(len(clusters[clusters[:,-1]==i])/len(pixels))
+	    
+	    for pixel in pixels:
+	    	for gaussian in range(k):
+	    		likelihood = multivariate_normal.pdf(pixel, initial_means[gaussian], initial_variances[gaussian])
+		
+		'''
+        for pixel in pixels:
+			likelihood = [multivariate_normal.pdf(pixel, initial_means[gaussian], initial_variances[gaussian]) for gaussian in range(k)]
+	    '''
+	    return likelihood
+
+    else:
+    	pass
 img = io.imread('stadium.bmp')
-print(EMG(img, 5, 0))
+print(EMG(img, 3, 0))

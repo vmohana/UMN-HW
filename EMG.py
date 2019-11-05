7f87e0683a89ac2bb396ddcdbe9123a9b3407dff
vmohana <36269935+vmohana@users.noreply.github.com>
2019-11-04 21:44:13 -0600

    Update EMG.py

--- EMG.py
+++ EMG.py
@@ -9,19 +9,16 @@ def EMG(image, k, flag):
     if flag == 0:
         image = image[:,:,:3]/255
         pixels = np.reshape(image[:,:,:3], (image.shape[0]*image.shape[1], 3))
-        colours = KMeans(n_clusters = k, max_iter = 3).fit(pixels)
+        colours = KMeans(n_clusters = k).fit(pixels)
         clusters = np.hstack((pixels, np.reshape(colours.labels_, (13400, 1))))
         means = colours.cluster_centers_
         
         # Calculate the initial variances
         variances = [np.cov(clusters[clusters[:,-1]==i][:,:-1], rowvar = False) for i in range(k)]
         pi = [len(clusters[clusters[:,-1]==i])/len(pixels) for i in range(k)]
-
-        
         expectation = None
         
-        for iterations in range(150):
-
+        for iterations in range(4):
             for pixel in range(len(pixels)):
 
                 # Expectation step
@@ -34,11 +31,16 @@ def EMG(image, k, flag):
                 means = [np.mean(clusters[clusters[:,-1]==i][:,:3], axis = 0) for i in range(k)]
                 variances = [np.cov(clusters[clusters[:,-1] == i][:,:-1], rowvar = False) for i in range(k)]
 
-            print(means)
-        print('Final means')
+            print(iterations,':',means)
 
+        compressed_image = np.array(means)[clusters[:,-1].astype(int)]
+        compressed_image = np.reshape(compressed_image, (image.shape[0],image.shape[1], 3))
+        plt.imshow(compressed_image)
+        plt.show()
         return means
-
+    
+    if flag == 1:
+        pass
 
 img = io.imread('stadium.bmp')
-print(EMG(img, 3, 0))
+print(EMG(img, 4, 0))

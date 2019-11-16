e442a6e8c30a3f35642601d94b774a6036584450
vmohana <36269935+vmohana@users.noreply.github.com>
2019-11-15 19:30:53 -0600

    Update dlist.py

--- dlist.py
+++ dlist.py
@@ -1,15 +1,11 @@
 import pandas as pd
+import math
+#from sklearn import tree
 
 # Preprocess the dataset
 dataset = pd.read_csv('dataset.csv', header=None)
 dataset.columns = ['Alt', 'Bar', 'Fri', 'Hun', 'Pat', 'Price', 'Rain', 'Res', 'Type', 'Est', 'Wait']
 dataset[dataset.columns] = dataset.apply(lambda x: x.str.strip())
-
-def split_data(dataset, column):
-    split_data = {}
-    for value in dataset[column].unique():
-        split_data[value] = dataset[dataset[column] == value]
-    return split_data
 
 def pure(dataset):
     if len(dataset['Wait'].unique()) == 1:
@@ -17,32 +13,21 @@ def pure(dataset):
     else:
         return False
 
+def split_set(dataset, attribute):
+    split = {}
+    for vk in dataset[attribute].unique():
+        split[vk] = dataset[dataset[attribute] == vk]
+    return split
 
-def decision_list(dataset):
-    if len(dataset) == 0:
-        return 
-    else:
-        split_attribute = None
-        split_value = None
-        for attribute in dataset.columns[0:10]:
-            sub_data = split_data(dataset, attribute)
-            for value in list(sub_data.keys()):
-                split = None
-                if pure(sub_data[value]):
-                    split_attribute = attribute
-                    split_value = value
-                    break
-                break
-        if dataset[dataset[split_attribute] == split_value]['Wait'].iloc[0] == 'T':
-            O = 'Yes'
-        else:
-            O = 'No'
-        print(split_value, split_attribute, O)
-        
-        dataset = dataset.drop(dataset[dataset[split_attribute] == split_value].index)
-        print(dataset)
-        return (split_attribute, split_value, O), decision_list(dataset)
-
-dlist = decision_list(dataset)
-print(dlist)
-print(dlist[0], dlist[1][0])
+def decision_list(examples):
+    d_list = []
+    while True:
+        for attribute in examples.columns[:-1]:
+            split_data = split_set(examples, attribute)
+            for vk in list(split_data.keys()):
+                if pure(split_data[vk]):
+                    d_list.append((attribute, vk, split_data[vk]['Wait'].iloc[0]))
+                    examples = examples.drop(examples[examples[attribute] == vk].index)
+                if len(examples) == 0:
+                    return d_list
+    return d_list
